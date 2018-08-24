2d3fa53f3ec39be42e8700de37f36b19653d4c5b
Rifhice <rifhice@MSI.localdomain>
2018-08-24 04:20:31 +0200

    get action queue

--- Route/Action_queue.js
+++ Route/Action_queue.js
@@ -2,9 +2,15 @@ var express = require("express");
 const router = express.Router();
 const Action_queue = require("../Database/Collection/Action_queue");
 
+router.get("/", function(req, res) {
+  Action_queue.getAll(data => {
+    res.send(data.val());
+  });
+});
+
 router.post("/", function(req, res) {
   Action_queue.write(req.body)
-    .then(() => res.send({ result: "success" }))
+    .then(() => res.send({ result: "success", code: 200 }))
     .catch(() => res.send({ result: "error" }));
 });
 
